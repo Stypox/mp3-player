9df1d0110fc2350879602edc1c95eb2cdc43ffc2
Stypox <stypox@pm.me>
2019-02-04 21:59:40 +0100

    Added arguments and support for Favourites
    Command line options are explained in the help screen.
    Favourites is a playlist that does not refer to a directory, but to a list of files. That list of files is saved, toghether with play order and start song, in the FAVOURITES_FILENAME.

--- mp3-player/mp3-player.py
+++ mp3-player/mp3-player.py
@@ -19,10 +19,12 @@ import random
 from time import sleep
 import sys
 from enum import Enum, Flag
+import argparse
 
 
 SETTINGS_FILENAME = "mp3-player-settings.txt"
 DIRECTORIES_FILENAME = "mp3-player-directories.txt"
+FAVOURITES_FILENAME = "mp3-player-favourites.txt"
 
 PATH_ORDER_CODES = ["0", "p", "path"]
 TITLE_ORDER_CODES = ["1", "t", "title", "name"]
@@ -89,6 +91,73 @@ class Options:
 	quiet = False
 	limitToConsoleWidth = False
 	consoleWidth = int(os.popen('stty size', 'r').read().split()[1])
+	playlists = []
+
+	argParser = argparse.ArgumentParser(prog="mp3-player.py")
+	argParser.add_argument('-q', '--quiet', action='store_true', default=False, help="do not print anything")
+	argParser.add_argument('-v', '--verbose', action='store_true', default=False, help="print more debug information")
+	argParser.add_argument('-w', '--limit-to-console-width', action='store_true', default=False, help="print to the console only part of the output so that it can fit in the console width")
+	argParser.add_argument('-o', '--favourites-play-order', type=str, default=None, help="favourites play order. Must match [m|modified]-(p|path|t|title|a|artist|n|number|tracknumber|r|random)")
+	argParser.add_argument('-s', '--favourites-start-song', type=int, default=None, help="favourites start index")
+	argParser.add_argument('playlists', nargs='*', metavar='DIRECTORIES', help="playlists to play (DIRECTORY) starting from INDEX (defaults to 0). FORMAT must match [m|modified]-(p|path|t|title|a|artist|n|number|tracknumber|r|random) (defaults to random). Formatted this way: DIRECTORY [FORMAT] [INDEX] - ... - DIRECTORY [FORMAT] [INDEX]")
+
+	@staticmethod
+	def parseArgsList(args, allArgs):
+		try:
+			if len(args) == 0:
+				return None
+			elif len(args) == 1:
+				return Playlist(args[0])
+			elif len(args) == 2:
+				return Playlist(args[0], args[1])
+			elif len(args) == 3:
+				return Playlist(args[0], args[1], args[2])
+			else:
+				raise RuntimeError("Invalid arguments (list of arguments \"%s\" too long): \"%s\"" % (args, allArgs))
+		except Playlist.EmptyDirectory as e:
+			log(LogLevel.warning, e.what())
+			return None
+
+	@staticmethod
+	def parse(arguments):
+		arguments = arguments[1:]
+		opts = vars(Options.argParser.parse_args(arguments))
+		Options.quiet = opts['quiet']
+		Options.verbose = opts['verbose']
+		Options.limitToConsoleWidth = opts['limit_to_console_width']
+
+		favouritesPlayOrder = opts['favourites_play_order']
+		favouritesStartSong = opts['favourites_start_song']
+
+		Favourites.setup(favouritesPlayOrder, favouritesStartSong)
+
+		playlistArgs = opts['playlists']
+		if len(playlistArgs) == 0:
+			try:
+				with open(DIRECTORIES_FILENAME) as directoriesFile:
+					for line in directoriesFile:
+						playlist = Options.parseArgsList(line, DIRECTORIES_FILENAME)
+						if type(playlist) is Playlist:
+							Options.playlists.append(playlist)
+					if len(playlist) == 0:
+						log(LogLevel.warning, "Empty file \"%s\"" % DIRECTORIES_FILENAME)
+			except:
+				log(LogLevel.warning, "No command line arguments and no \"%s\" file found: using current directory" % DIRECTORIES_FILENAME)
+				try: Options.playlists.append(Playlist("./"))
+				except Playlist.EmptyDirectory as e: log(LogLevel.warning, e.what())
+		else:
+			tmpArgs = []
+			for arg in playlistArgs:
+				if arg == "-":
+					playlist = Options.parseArgsList(tmpArgs, playlistArgs)
+					if type(playlist) is Playlist:
+						Options.playlists.append(playlist)
+					tmpArgs = []
+				else:
+					tmpArgs.append(arg)
+			playlist = Options.parseArgsList(tmpArgs, playlistArgs)
+			if type(playlist) is Playlist:
+				Options.playlists.append(playlist)
 
 class LogLevel(Enum):
 	debug = 0,
@@ -269,7 +338,7 @@ class Song:
 	def __init__(self, path):
 		self.path = path
 		try: self.songID3 = EasyID3(path)
-		except: pass
+		except: log(LogLevel.debug, "Unable to read ID3 tags for song at \"%s\"" % path)
 	def __repr__(self):
 		try:
 			return self.songID3["title"][0]
@@ -292,6 +361,69 @@ class Song:
 		except:
 			return Song.invalidTrackNumber
 
+class Favourites:
+	@staticmethod
+	def setup(playOrder, startSong):
+		songFilenames, filePlayOrder, fileStartSong = Favourites.loadFromFile()
+
+		Favourites.songs = [Song(songFilename) for songFilename in songFilenames]
+		if len(Favourites.songs) == 0:
+			log(LogLevel.warning, "Favourites playlist is empty")
+		
+		if playOrder is None:
+			if filePlayOrder is None:
+				Favourites.playOrder = Order.default
+			else:
+				Favourites.playOrder = filePlayOrder
+		else:
+			Favourites.playOrder = Order.cast(playOrder)
+			if playOrder is not None and Favourites.playOrder is None:
+				raise RuntimeError("Invalid play order \"%s\" for favourites of type \"%s\"" % (playOrder, type(playOrder)))
+		
+		if startSong is None:
+			if fileStartSong is None:
+				Favourites.currentSong = 0
+			else:
+				Favourites.currentSong = fileStartSong
+		else:
+			Favourites.playOrder = startSong
+		#this is done since __next__ does += 1 even the first time
+		Favourites.currentSong -= 1
+	def __iter__(self):
+		return self
+	def __next__(self):
+		Favourites.currentSong += 1
+		Favourites.currentSong %= len(Favourites.songs)
+		return Favourites.songs[Favourites.currentSong]
+
+	@staticmethod
+	def loadFromFile():
+		try:
+			with open(FAVOURITES_FILENAME) as favouritesFile:
+				playOrder = favouritesFile.readline().strip()
+				startSong = favouritesFile.readline().strip()
+
+				playOrder = Order.cast(playOrder)
+				try: startSong = int(startSong)
+				except ValueError: startSong = None
+
+				songFilenames = [line.strip() for line in favouritesFile]
+
+				return (songFilenames, playOrder, startSong)
+		except FileNotFoundError:
+			return ([], None, None)
+	@staticmethod
+	def writeSettings():
+		with open(FAVOURITES_FILENAME, "w") as favouritesFile:
+			if Favourites.playOrder == Order.random:
+				favouritesFile.write("%s\n%s\n" % (Favourites.playOrder.value, 0))
+			else:
+				favouritesFile.write("%s\n%s\n" % (Favourites.playOrder.value, Favourites.currentSong))
+			favouritesFile.write("\n".join([song.path for song in Favourites.songs]))
+	@staticmethod
+	def add(song):
+		Favourites.songs.append(song)
+
 class Playlist:
 	class EmptyDirectory(BaseException):
 		def __init__(self, directory):
@@ -299,44 +431,57 @@ class Playlist:
 		def what(self):
 			return "Provided directory %s is empty" % self.directory
 
-	def __init__(self, directory, playOrder = None, startSong = None):
-		if len(directory) > 0 and directory[-1] != "/":
-			directory += "/"
-		self.directory = directory
-		
-		self.playOrder = Order.cast(playOrder)
-		if playOrder is not None and self.playOrder is None:
-			raise RuntimeError("Invalid play order \"%s\" at playlist \"%s\" of type \"%s\"" % (playOrder, self.directory, type(playOrder)))
+	def __init__(self, directoryOrFilenames, playOrder = None, startSong = None):
+		if type(directoryOrFilenames) is str:
+			if len(directoryOrFilenames) > 0 and directoryOrFilenames[-1] != "/":
+				directoryOrFilenames += "/"
+			self.directory = directoryOrFilenames
+			
+			self.playOrder = Order.cast(playOrder)
+			if playOrder is not None and self.playOrder is None:
+				raise RuntimeError("Invalid play order \"%s\" at playlist \"%s\" of type \"%s\"" % (playOrder, self.directory, type(playOrder)))
 
-		try:
-			self.currentSong = int(startSong)
-		except TypeError:
-			self.currentSong = None
-		if startSong is not None and self.currentSong is None:
-			raise RuntimeError("Invalid start song \"%s\" at playlist \"%s\" of type \"%s\"" % (startSong, self.directory, type(playOrder)))
-
-		if self.playOrder is None or self.currentSong is None:
 			try:
-				settingsFile = open(self.directory + SETTINGS_FILENAME, "r")
-				filePlayOrder = settingsFile.readline().strip()
-				fileStartSong = settingsFile.readline().strip()
+				self.currentSong = int(startSong)
+			except TypeError:
+				self.currentSong = None
+			if startSong is not None and self.currentSong is None:
+				raise RuntimeError("Invalid start song \"%s\" at playlist \"%s\" of type \"%s\"" % (startSong, self.directory, type(playOrder)))
 
+			if self.playOrder is None or self.currentSong is None:
+				try:
+					with open(self.directory + SETTINGS_FILENAME) as settingsFile:
+						filePlayOrder = settingsFile.readline().strip()
+						fileStartSong = settingsFile.readline().strip()
+
+						if self.playOrder is None:
+							self.playOrder = Order.cast(filePlayOrder)
+						if self.currentSong is None:
+							try: self.currentSong = int(fileStartSong)
+							except ValueError: pass
+				except FileNotFoundError: pass
+					
 				if self.playOrder is None:
-					self.playOrder = Order.cast(filePlayOrder)
-				if self.currentSong is None:
-					try:
-						self.currentSong = int(fileStartSong)
-					except ValueError: pass
-			except FileNotFoundError: pass
-				
-			if self.playOrder is None:
-				self.playOrder = Order.default
-			if self.currentSong is None or self.playOrder is Order.random:
-				self.currentSong = 0
+					self.playOrder = Order.default
+				if self.currentSong is None or self.playOrder is Order.random:
+					self.currentSong = 0
 
-		self.name = self.directory.split("/")[-1]
+			self.name = self.directory.split("/")[-1]
 
-		self.loadSongs()
+			self.loadSongs()
+		elif type(directoryOrFilenames) is list:
+			self.playOrder = Order.cast(playOrder)
+			if playOrder is not None and self.playOrder is None:
+				raise RuntimeError("Invalid play order \"%s\" for favourites of type \"%s\"" % (playOrder, type(playOrder)))
+
+			self.currentSong = startSong
+
+			self.songs = []
+			for filename in directoryOrFilenames:
+				self.songs.append(Song(filename))			
+		else:
+			raise TypeError()
+
 		self.sort()
 		#this is done since __next__ does += 1 even the first time
 		self.currentSong -= 1
@@ -356,11 +501,11 @@ class Playlist:
 		if len(self.songs) == 0:
 			raise Playlist.EmptyDirectory(self.directory)
 	def writeSettings(self):
-		settingsFile = open(self.directory + SETTINGS_FILENAME, "w")
-		if self.playOrder == Order.random:
-			settingsFile.write("%s\n%s" % (self.playOrder.value, 0))
-		else:
-			settingsFile.write("%s\n%s" % (self.playOrder.value, self.currentSong))
+		with open(self.directory + SETTINGS_FILENAME, "w") as settingsFile:
+			if self.playOrder == Order.random:
+				settingsFile.write("%s\n%s" % (self.playOrder.value, 0))
+			else:
+				settingsFile.write("%s\n%s" % (self.playOrder.value, self.currentSong))
 	def sort(self, playOrder = None):
 		if playOrder is None:
 			playOrder = self.playOrder
@@ -381,14 +526,26 @@ class Playlist:
 				for i in range(0, len(self.songs) - 5, 4):
 					self.songs[i:i+5] = sorted(self.songs[i:i+5], key=lambda s: random.random())
 
-	def play(self):
-		for song in self:
+class PlaylistsPlayer:
+	class Event(Enum):
+		next = 0
+		prev = 1
+		save = 2
+		abort = 3
+
+	def __init__(self, playlists):
+		self.playlists = playlists
+		self.currentPlaylist = 0
+	
+	@staticmethod
+	def playPlaylist(playlist):
+		for song in playlist:
 			player = vlc.MediaPlayer(song.path)
 			player.play()
 			if song.artist() is Song.invalidArtist:
-				log(LogLevel.info, "Playing %d/%d: \"%s\"" % (self.currentSong + 1, len(self.songs), song.title()))
+				log(LogLevel.info, "Playing %d/%d: \"%s\"" % (playlist.currentSong + 1, len(playlist.songs), song.title()))
 			else:
-				log(LogLevel.info, "Playing %d/%d: \"%s\" by \"%s\"" % (self.currentSong + 1, len(self.songs), song.title(), song.artist()))
+				log(LogLevel.info, "Playing %d/%d: \"%s\" by \"%s\"" % (playlist.currentSong + 1, len(playlist.songs), song.title(), song.artist()))
 			paused = False
 
 			while player.get_state() != vlc.State.Ended:
@@ -409,7 +566,7 @@ class Playlist:
 				elif nextAction == Keyboard.Event.restart:
 					player.stop()
 					#this is done instead of = 0 since __next__ does += 1
-					self.currentSong = -1
+					playlist.currentSong = -1
 					log(LogLevel.info, "Restart")
 					break
 				elif nextAction == Keyboard.Event.nextSong:
@@ -418,30 +575,20 @@ class Playlist:
 				elif nextAction == Keyboard.Event.prevSong:
 					player.stop()
 					#this is done instead of -= 1 since __next__ does += 1
-					self.currentSong -= 2
+					playlist.currentSong -= 2
 					break
 				elif nextAction == Keyboard.Event.nextPlaylist:
 					player.stop()
 					#this is done since __next__ does += 1 even the first time
-					self.currentSong -= 1
+					playlist.currentSong -= 1
 					return PlaylistsPlayer.Event.next
 				elif nextAction == Keyboard.Event.prevPlaylist:
 					player.stop()
 					#this is done since __next__ does += 1 even the first time
-					self.currentSong -= 1
+					playlist.currentSong -= 1
 					return PlaylistsPlayer.Event.prev
 		return PlaylistsPlayer.Event.next
 
-class PlaylistsPlayer:
-	class Event(Enum):
-		next = 0
-		prev = 1
-		save = 2
-		abort = 3
-
-	def __init__(self, playlists):
-		self.playlists = playlists
-		self.currentPlaylist = 0
 	
 	def play(self):
 		nrPlaylists = len(self.playlists)
